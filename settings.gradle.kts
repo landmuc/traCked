14e34e1f84e99975db44a2cd2dd5733eff6721f3
landmuc <landmucdev@gmail.com>
2024-01-05 19:12:03 +0100

    Added moular structure and buildSrc gradle file

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,3 +15,11 @@ dependencyResolutionManagement {
 
 rootProject.name = "traCked"
 include(":app")
+include(":core")
+include(":questionnaire")
+include(":questionnaire:questionnaire_presentation")
+include(":questionnaire:questionnaire_domain")
+include(":tracker")
+include(":tracker:tracker_data")
+include(":tracker:tracker_domain")
+include(":tracker:tracker_presentation")
